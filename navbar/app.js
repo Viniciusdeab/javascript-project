f115012b9bb66625e7c248dbf68fc411add16e36
unknown <viniciusdeab@outlook.com>
2022-07-27 13:16:26 -0300

    Descrição da propriedade do DOM e criação das variaveis para acessar as classe que receberão a dinamica do javascript

---
+++ navbar/app.js
@@ -0,0 +1,12 @@
+// classList - para mostrar/pegar todas as classes;
+// contains - verifica o classList para uma classe específica;
+// add - adiciona classes;
+// remove - remove classes;
+// toggle - para adicionar a alternar na classe; 
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function(){
+    
+})
