b38eb03e1e38bdcbb408cf2c34e9c7f16438210e
unknown <viniciusdeab@outlook.com>
2022-07-29 13:55:49 -0300

    Adição da dinamica do botão com toggle e ajuste no index.hmlt

---
+++ sidebar/app.js
@@ -0,0 +1,16 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const cleseBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function(){
+    // if(sidebar.classList.contains("show-sidebar")){
+    //     sidebar.classList.remove("show-sidebar");
+    // } else {
+    //     sidebar.classList.add("show-sidebar");
+    // }
+    sidebar.classList.toggle("show-sidebar");
+});
+
+cleseBtn.addEventListener("click", function(){
+    sidebar.classList.remove("show-sidebar");
+});
